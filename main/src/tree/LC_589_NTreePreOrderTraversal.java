5acdc968237824b197bfc233429eb39f4dd37a19
梁仟 <liangqian0723@gmail.com>
2019-10-27 09:00:31 +0800

    #589 N叉树前序遍历，递归版本和栈迭代版本 注：做完了n叉树的前序遍历和后序遍历，但是n叉树没有 中序遍历，因为多子节点的情况下，不能确定根结点在何处输出

---
+++ main/src/tree/LC_589_NTreePreOrderTraversal.java
@@ -0,0 +1,48 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * @author alis
+ * @date 2019/10/27 8:43 AM
+ * @description n叉树的前序遍历
+ */
+public class LC_589_NTreePreOrderTraversal {
+    /**
+     * 递归遍历n叉树
+     */
+    public List<Integer> preOrderTraversalRecur(NTreeNode root) {
+        List<Integer> nums = new ArrayList<>();
+        if (root == null) {
+            return nums;
+        }
+        nums.add(root.val);
+        if (root.children != null && !root.children.isEmpty()) {
+            for (NTreeNode child : root.children) {
+                nums.addAll(preOrderTraversalRecur(child));
+            }
+        }
+        return nums;
+    }
+
+    public List<Integer> preOrderTraversalStack(NTreeNode root) {
+        List<Integer> nums = new ArrayList<>();
+        if (root == null) {
+            return nums;
+        }
+        Stack<NTreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            NTreeNode node = stack.pop();
+            nums.add(node.val);
+            if (node.children != null && !node.children.isEmpty()) {
+                Collections.reverse(node.children);
+                node.children.forEach(stack::push);
+            }
+        }
+        return nums;
+    }
+}

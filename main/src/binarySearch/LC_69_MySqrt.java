7c835ceacc051c9f8ed8572950d8c35c4a6b4a3d
梁仟 <liangqian0723@gmail.com>
2019-11-03 17:10:18 +0800

    #69 计算平方根：二分遍历

---
+++ main/src/binarySearch/LC_69_MySqrt.java
@@ -0,0 +1,28 @@
+package binarySearch;
+
+/**
+ * @author alis
+ * @date 2019/11/3 3:55 PM
+ * @description
+ */
+public class LC_69_MySqrt {
+
+    /**
+     * 二分查找的方式遍历计算X的平方根
+     * 思路：二分法，从0到x/2+1 开始，利用二分法去遍历各个二分法情况
+     * 注意：二分法去计算中位数的时候，会出现取整偏差的情况，(left + right) / 2 -> (left + right + 1)/2
+     * 并且当left + right超出范围了，会出现溢出的情况，所以用long类型取运算，
+     */
+    public int mySqrtBinarySearch(int x) {
+        long left = 0, right = x / 2 + 1;
+        while (left < right) {
+            long mid = (left + right + 1) >>> 1;
+            if (mid * mid > x) {
+                right = mid - 1;
+            } else {
+                left = mid;
+            }
+        }
+        return (int) left;
+    }
+}
---
+++ main/test/binarySearch/LC_69_MySqrtTest.java
@@ -0,0 +1,23 @@
+package binarySearch;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author alis
+ * @date 2019/11/3 3:53 PM
+ * @description
+ */
+public class LC_69_MySqrtTest {
+
+    private LC_69_MySqrt mySqrt = new LC_69_MySqrt();
+
+    @Test
+    public void mySqrtTest(){
+        assertThat(mySqrt.mySqrtBinarySearch(4), is(2));
+        assertThat(mySqrt.mySqrtBinarySearch(8), is(2));
+        assertThat(mySqrt.mySqrtBinarySearch(2147395599), is(46339));
+    }
+}

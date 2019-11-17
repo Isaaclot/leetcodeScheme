633042dbeace26f6adeae418f5b5f118d7e268c9
梁仟 <liangqian0723@gmail.com>
2019-11-17 22:06:58 +0800

    #63 路径计算 带路障，在设置边路值的时候，需要根据路障和上一步的值来设置

---
+++ main/test/dynamic_programming/LC_63_UniquePathsWithObstaclesTest.java
@@ -0,0 +1,33 @@
+package dynamic_programming;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author alis
+ * @date 2019/11/17 2:12 AM
+ * @description
+ */
+public class LC_63_UniquePathsWithObstaclesTest {
+
+    private LC_63_UniquePathsWithObstacles uniquePathsWithObstaclesDomain = new LC_63_UniquePathsWithObstacles();
+
+    @Test
+    public void testUniquePathsWithObstacles() {
+//        assertThat(uniquePathsWithObstaclesDomain.uniquePathsWithObstacles(new int[][]{
+//                {0, 0, 0},
+//                {0, 1, 0},
+//                {0, 0, 0}
+//        }), is(2));
+//
+//        assertThat(uniquePathsWithObstaclesDomain.uniquePathsWithObstacles(new int[][]{
+//                {1, 0}
+//        }), is(0));
+
+        assertThat(uniquePathsWithObstaclesDomain.uniquePathsWithObstacles(new int[][]
+                {{0, 0}, {1, 1}, {0, 0}}
+        ), is(0));
+    }
+}

001e2393851dd7425487dfd2be35a15fab292361
lqian <liangqian0723@gmail.com>
2020-04-24 14:12:45 +0800

    #120 dp 三角形最小路径和

--- main/src/dynamic_programming/LC_120_MinimumTotal.java
+++ main/src/dynamic_programming/LC_120_MinimumTotal.java
@@ -80,4 +80,54 @@ public class LC_120_MinimumTotal {
         }
         return dp[0][0];
     }
+
+    /**
+     * dp方程:
+     * i==j ? dp[i][j] = triangle.get(i).get(j) + dp[i - 1][j - 1];
+     * i!=j ? dp[i][j] = triangle.get(i).get(j) + Math.min(dp[i - 1][j], dp[i - 1][j - 1]);
+     */
+    public int minimumTotal4(List<List<Integer>> triangle) {
+        int row = triangle.size();
+        int col = triangle.get(row - 1).size();
+        int[][] dp = new int[row][col];
+        // init
+        dp[0][0] = triangle.get(0).get(0);
+        for (int i = 1; i < row; i++) {
+            dp[i][0] = triangle.get(i).get(0) + dp[i - 1][0];
+        }
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (i == j) {
+                    dp[i][j] = triangle.get(i).get(j) + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = triangle.get(i).get(j) + Math.min(dp[i - 1][j], dp[i - 1][j - 1]);
+                }
+            }
+        }
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < col; i++) {
+            min = Math.min(min, dp[row - 1][i]);
+        }
+        return min;
+    }
+
+
+    /**
+     * 一维dp方程, 自底向上
+     */
+    public int minimumTotal4_1D(List<List<Integer>> triangle) {
+        int row = triangle.size();
+        int col = triangle.get(row - 1).size();
+        int[] dp = new int[col];
+        // init
+        for (int i = 0; i < col; i++) {
+            dp[i] = triangle.get(row - 1).get(i);
+        }
+        for (int i = row - 2; i >= 0; --i) {
+            for (int j = 0; j <= i; j++) {
+                dp[j] = triangle.get(i).get(j) + Math.min(dp[j + 1], dp[j]);
+            }
+        }
+        return dp[0];
+    }
 }
--- main/test/dynamic_programming/LC_120_MinimumTotalTest.java
+++ main/test/dynamic_programming/LC_120_MinimumTotalTest.java
@@ -48,4 +48,24 @@ public class LC_120_MinimumTotalTest {
         input.add(Arrays.asList(4, 1, 8, 3));
         assertThat(mt.minimumTotal3(input), is(11));
     }
+
+    @Test
+    public void minimumTotal4() {
+        List<List<Integer>> input = new ArrayList<>();
+        input.add(Collections.singletonList(2));
+        input.add(Arrays.asList(3, 4));
+        input.add(Arrays.asList(6, 5, 7));
+        input.add(Arrays.asList(4, 1, 8, 3));
+        assertThat(mt.minimumTotal4(input), is(11));
+    }
+
+    @Test
+    public void minimumTotal4_1D() {
+        List<List<Integer>> input = new ArrayList<>();
+        input.add(Collections.singletonList(2));
+        input.add(Arrays.asList(3, 4));
+        input.add(Arrays.asList(6, 5, 7));
+        input.add(Arrays.asList(4, 1, 8, 3));
+        assertThat(mt.minimumTotal4_1D(input), is(11));
+    }
 }

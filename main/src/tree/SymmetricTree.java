8f09b3c5ee6b624e0552ea6778663f8869ef2353
lqian <liangqian0723@gmail.com>
2020-05-31 22:31:57 +0800

    #101 对称二叉树迭代法

--- main/src/tree/SymmetricTree.java
+++ main/src/tree/SymmetricTree.java
@@ -1,5 +1,8 @@
 package tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @author liangqian@vvic.com
  * @version 1.0
@@ -26,5 +29,27 @@ public class SymmetricTree {
         // drill down
         return isSymmetric(leftTree.left, rightTree.right) && isSymmetric(leftTree.right, rightTree.left);
 
+    }
+
+    public boolean isSymmetric_loop(TreeNode treeRoot) {
+        if (treeRoot == null) return true;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(treeRoot);
+        queue.add(treeRoot);
+        while (!queue.isEmpty()) {
+            TreeNode poll1 = queue.poll();
+            TreeNode poll2 = queue.poll();
+            if (poll1 != null && poll2 != null && poll1.val == poll2.val) {
+                queue.add(poll1.left);
+                queue.add(poll2.right);
+                queue.add(poll1.right);
+                queue.add(poll2.left);
+            } else if (poll1 == null && poll2 == null) {
+
+            } else {
+                return false;
+            }
+        }
+        return treeRoot != null;
     }
 }
--- main/test/tree/SymmetricTreeTest.java
+++ main/test/tree/SymmetricTreeTest.java
@@ -29,6 +29,21 @@ public class SymmetricTreeTest {
     }
 
     @Test
+    public void should_is_symmetric_tree_loop() {
+        SymmetricTree st = new SymmetricTree();
+        TreeNode treeRoot = new TreeNode(1);
+        TreeNode left2 = new TreeNode(2);
+        TreeNode right2 = new TreeNode(2);
+        treeRoot.left = left2;
+        treeRoot.right = right2;
+        left2.left = new TreeNode(3);
+        left2.right = new TreeNode(4);
+        right2.left = new TreeNode(4);
+        right2.right = new TreeNode(3);
+        assertThat(st.isSymmetric_loop(treeRoot), is(true));
+    }
+
+    @Test
     public void should_is_symmetric_tree_case_2() {
         SymmetricTree st = new SymmetricTree();
         TreeNode treeRoot = new TreeNode(2);
@@ -41,5 +56,20 @@ public class SymmetricTreeTest {
         right2.right = new TreeNode(4);
         assertThat(st.isSymmetric(treeRoot), is(false));
     }
+
+    @Test
+    public void should_is_symmetric_tree_case_2_loop_queue() {
+        SymmetricTree st = new SymmetricTree();
+        TreeNode treeRoot = new TreeNode(2);
+        TreeNode left2 = new TreeNode(3);
+        TreeNode right2 = new TreeNode(3);
+        treeRoot.left = left2;
+        treeRoot.right = right2;
+        left2.left = new TreeNode(4);
+        left2.right = new TreeNode(5);
+        right2.right = new TreeNode(4);
+        assertThat(st.isSymmetric_loop(treeRoot), is(false));
+    }
+
 
 }
